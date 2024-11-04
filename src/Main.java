4a70fe1fedb1d55d433c47c36f00f450faa05c9a
yizuoliang <yi_zuoliang@163.com>
2024-11-04 16:08:20 +0800

    首次提交

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName: ${NAME}
+ * @Description:
+ * @Author: yizuoliang
+ * @Date: 2024/11/4 15:58
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
